f6adaa582c0be2d13780ecd0d406711939ff986c
Eduard <eduardkiller@hotmail.com>
2020-06-04 21:52:52 -0300

    Testando Operador And &

--- src/net/eduard/curso/java/TesteOperadorAnd.java
+++ src/net/eduard/curso/java/TesteOperadorAnd.java
@@ -1,13 +1,17 @@
 package net.eduard.curso.java;
 
+import net.eduard.api.lib.game.FakePlayer;
+import net.eduard.api.lib.manager.CurrencyManager;
+
 public class TesteOperadorAnd {
 
     public static void main(String[] args) {
-            int max = 10;
+            int max = 20;
+        CurrencyManager
         for (int x= 1;x<=max;x++){
-
+            System.out.println("---------------------------------------");
             for (int y= 1;y<=max;y++){
-                System.out.println(""+x+" & "+y +" = "+ (x & y)+" | "+ (y & x)+" x= "+x +" y= "+y);
+                System.out.println("|   "+x+"   &   "+y +" =    ["+ (x & y)+" | "+ (y & x)+"]   x= "+x +" y= "+y+"   |");
             }
         }
     }
